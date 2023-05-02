e714906719c5cf4b3f409019de5b5e03f263ae36
Jean Claude Mbiya <46892936+johnmbiya@users.noreply.github.com>
2023-05-02 09:33:01 +0100

    Update CommandoServiceProvider.php

--- src/CommandoServiceProvider.php
+++ src/CommandoServiceProvider.php
@@ -3,6 +3,14 @@
 namespace Letecode\Commando;
 
 use Illuminate\Support\ServiceProvider;
+use Letecode\Commando\Commands\Files\CreateClass;
+use Letecode\Commando\Commands\Files\CreateFile;
+use Letecode\Commando\Commands\Files\CreateInterface;
+use Letecode\Commando\Commands\Files\CreateLang;
+use Letecode\Commando\Commands\Files\CreateRepository;
+use Letecode\Commando\Commands\Files\CreateService;
+use Letecode\Commando\Commands\Files\CreateTrait;
+use Letecode\Commando\Commands\Files\CreateView;
 
 class CommandoServiceProvider extends ServiceProvider
 {
@@ -21,6 +29,19 @@ class CommandoServiceProvider extends ServiceProvider
         $this->publishes([
             self::CONFIG_PATH => config_path('commando.php'),
         ], 'config');
+
+        if($this->app->runningInConsole()) {
+            $this->commands([
+                CreateClass::class,
+                CreateInterface::class,
+                CreateFile::class,
+                CreateLang::class,
+                CreateRepository::class,
+                CreateService::class,
+                CreateTrait::class,
+                CreateView::class,
+            ]);
+        }
     }
 
      /**
